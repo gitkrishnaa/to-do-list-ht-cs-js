9e51aee7b2833b331e4f20b63eeec7a5cc94a252
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2022-12-25 18:05:30 +0530

    1 file modified

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let display = document.getElementById('note-display');
 let input1 = document.getElementById('input');
 input1.setAttribute('placeholder', 'type your note');
 let add_note = document.getElementById('add_note');
-add_note.addEventListener('click', () => {
+let renderFunc=() => {
   if (input1.value != '') {
     let div1 = document.createElement('div');
     let div2 = document.createElement('div');
-div2.className="created_div2"
+    div2.className = 'created_div2';
 
     let delete_button = document.createElement('button');
     let note = document.createElement('h3');
@@ -17,12 +17,11 @@ div2.className="created_div2"
     let edit_button = document.createElement('button');
     edit_button.innerText = 'edit';
 
-    // append 
+    // append
     display.appendChild(div1);
     div1.appendChild(note);
     div1.appendChild(div2);
 
-    
     div2.appendChild(delete_button);
     div2.appendChild(edit_button);
     //edit section
@@ -34,17 +33,14 @@ div2.className="created_div2"
         edit_button.innerText = 'done';
         note.style.backgroundColor = 'RGB(192,192,199)';
         temp = false;
-       
       } else {
         note.setAttribute('contenteditable', 'false');
         edit_button.innerText = 'edit';
         temp = true;
         note.style.backgroundColor = 'white';
-        if(note.innerHTML==""){
+        if (note.innerHTML == '') {
           div1.remove();
-         
-       }
-        
+        }
       }
     });
 
@@ -63,17 +59,18 @@ div2.className="created_div2"
   } else {
     input1.setAttribute('placeholder', 'input field is empty');
   }
-});
+}
+let note_value=input1.value;
+let note_store_array=[]
+add_note.addEventListener("click",()=>{
+ if(localStorage.getItem==null){
+   localStorage.setItem("note_array","[]")
+ }
+//  note_store_array.push(input1.value);
 
-
-
-//////setting button
-
-
-
-
-
-
-
-
-
+  var old_data=JSON.parse(localStorage.getItem("note_array"))
+  old_data.push(input1.value);
+  localStorage.setItem("note_")
+  // temp=localStorage.getItem("note")+"$"
+  console.log(note_value)
+})
